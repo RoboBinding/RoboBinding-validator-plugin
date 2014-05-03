0a417bbde47a1f08f974cc5d1c5c0fd003024738
weicheng113 <weicheng112@gmail.com>
2014-05-03 18:12:17 +0800

    Fixed reference issues and applied single licence file.

--- src/test/groovy/org/robobinding/plugins/validator/XmlWithBindingAttributesTest.groovy
+++ src/test/groovy/org/robobinding/plugins/validator/XmlWithBindingAttributesTest.groovy
@@ -1,21 +1,6 @@
-/**
- * Copyright 2013 Cheng Wei, Robert Taylor
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- */
 package org.robobinding.plugins.validator
 
-import org.robobinding.binder.ViewNameResolver
+import org.robobinding.ViewNameResolver
 
 import spock.lang.Specification
 import android.widget.EditText
@@ -33,17 +18,17 @@ class XmlWithBindingAttributesTest extends Specification {
 	XmlWithBindingAttributes xmlWithBindingAttributes = new XmlWithBindingAttributes(
 		xmlLineNumberDecorator: xmlLineNumberDecorator,
 		viewNameResolver: new ViewNameResolver())
-	
+
 	def "given xml with binding attributes then determine view name and line numbers"() {
 		given:
 		String rawXml = "<raw-xml/>"
-		String xmlWithLineNumbers = 
+		String xmlWithLineNumbers =
 				  '''<?xml version="1.0" encoding="utf-8"?>
 					 <LinearLayout
 			            xmlns:android="http://schemas.android.com/apk/res/android"
 			            xmlns:bind="http://robobinding.org/android"
 			            android:orientation="horizontal">
-					    
+
 					    <TextView line_number="6" android:id="@+id/some_id"
 					       android:layout_width="fill_parent"
 					       android:layout_height="wrap_content"
@@ -52,7 +37,7 @@ class XmlWithBindingAttributesTest extends Specification {
 						<RelativeLayout line_number="11"
 						   android:layout_width="fill_parent"
 					       android:layout_height="wrap_content">
-					       
+
 						   <EditText line_number="15"
 					          android:layout_width="fill_parent"
 					          android:layout_height="wrap_content"
@@ -62,10 +47,10 @@ class XmlWithBindingAttributesTest extends Specification {
 						 </RelativeLayout>
 					 </LinearLayout>'''
 		xmlLineNumberDecorator.embedLineNumbers(rawXml, 'bind') >> xmlWithLineNumbers
-		
+
 		when:
 		List<ViewBindingAttributes> results = xmlWithBindingAttributes.findViewsWithBindings(rawXml, 'bind')
-		
+
 		then:
 		results.size() == 2
 		results[0].view instanceof TextView
@@ -79,7 +64,7 @@ class XmlWithBindingAttributesTest extends Specification {
 		results[1].bindingAttributes['visibility'].attributeValue == '{nameVisible}'
 		results[1].bindingAttributes['visibility'].lineNumber == 18
 	}
-	
+
 	def "should not [yet] process custom views"() {
 		given:
 		String rawXml = "<raw-xml/>"
@@ -89,7 +74,7 @@ class XmlWithBindingAttributesTest extends Specification {
 			            xmlns:android="http://schemas.android.com/apk/res/android"
 			            xmlns:bind="http://robobinding.org/android"
 			            android:orientation="horizontal">
-					    
+
 					    <org.custom.TextView line_number="6" android:id="@+id/some_id"
 					       android:layout_width="fill_parent"
 					       android:layout_height="wrap_content"
@@ -98,7 +83,7 @@ class XmlWithBindingAttributesTest extends Specification {
 						<RelativeLayout line_number="11"
 						   android:layout_width="fill_parent"
 					       android:layout_height="wrap_content">
-					       
+
 						   <org.custom.EditText line_number="15"
 					          android:layout_width="fill_parent"
 					          android:layout_height="wrap_content"
@@ -108,14 +93,14 @@ class XmlWithBindingAttributesTest extends Specification {
 						 </RelativeLayout>
 					 </LinearLayout>'''
 		xmlLineNumberDecorator.embedLineNumbers(rawXml, 'bind') >> xmlWithLineNumbers
-		
+
 		when:
 		List<ViewBindingAttributes> results = xmlWithBindingAttributes.findViewsWithBindings(rawXml, 'bind')
-		
+
 		then:
 		results.isEmpty()
 	}
-	
+
 	def "should process android views inside custom containers"() {
 		given:
 		String rawXml = "<raw-xml/>"
@@ -125,7 +110,7 @@ class XmlWithBindingAttributesTest extends Specification {
 			            xmlns:android="http://schemas.android.com/apk/res/android"
 			            xmlns:bind="http://robobinding.org/android"
 			            android:orientation="horizontal">
-					    
+
 					    <TextView line_number="6" android:id="@+id/some_id"
 					       android:layout_width="fill_parent"
 					       android:layout_height="wrap_content"
@@ -134,7 +119,7 @@ class XmlWithBindingAttributesTest extends Specification {
 						<org.custom.RelativeLayout line_number="11"
 						   android:layout_width="fill_parent"
 					       android:layout_height="wrap_content">
-					       
+
 						   <EditText line_number="15"
 					          android:layout_width="fill_parent"
 					          android:layout_height="wrap_content"
@@ -144,10 +129,10 @@ class XmlWithBindingAttributesTest extends Specification {
 						 </org.custom.RelativeLayout>
 					 </org.custom.LinearLayout>'''
 		xmlLineNumberDecorator.embedLineNumbers(rawXml, 'bind') >> xmlWithLineNumbers
-		
+
 		when:
 		List<ViewBindingAttributes> results = xmlWithBindingAttributes.findViewsWithBindings(rawXml, 'bind')
-		
+
 		then:
 		results.size() == 2
 	}
